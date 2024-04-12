67a99adebc7b35feb8dc904ad54f0c7851524739
brunortrocoli <164953384+brunortrocoli@users.noreply.github.com>
2024-04-12 20:19:06 +0000

    v1.3

--- alchimia/alchimia.js
+++ alchimia/alchimia.js
@@ -19,19 +19,21 @@ $(window).on('load', function () {
 ///////////////////////////////////////////////
 
 var velocity = 600;
-jQuery(document).ready(function ($) {
-  $('a[href^="#"]').bind('click.smoothscroll', function (e) {
-    e.preventDefault();
-    var target = this.hash,
-      $target = $(target);
-
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top - 45
-    }, velocity, function () {
-      window.location.hash = target;
+function scroll(){
+  jQuery(document).ready(function ($) {
+    $('a[href^="#"]').bind('click.smoothscroll', function (e) {
+      e.preventDefault();
+      var target = this.hash,
+        $target = $(target);
+  
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 5
+      }, velocity, function () {
+        window.location.hash = target;
+      });
     });
   });
-});
+}
 
 ////////////////////////////////////////////////
 
